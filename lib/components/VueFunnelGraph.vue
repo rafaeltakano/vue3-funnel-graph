0006fc63aeea5d195638cf1c2a2c0bbc58cffc79
Rafael Takano <rafael-takano@hotmail.com>
2022-08-28 15:48:58 -0300

    feat: created component methods

--- lib/components/VueFunnelGraph.vue
+++ lib/components/VueFunnelGraph.vue
@@ -58,4 +58,48 @@ const graph = ref(null);
 const paths = ref([]);
 const prevPaths = ref([]);
 const defaultColors = ref(getDefaultColors(10));
+
+const is2d = () => {
+  if (graph.value?.is2d) return graph.value.is2d();
+  throw new TypeError("Variable graph is not instance of VueFunnelGraph");
+};
+
+const getPathDefinitions = () => {
+  if (graph.value?.getPathDefinitions) return graph.value.getPathDefinitions();
+  throw new TypeError("Variable graph is not instance of VueFunnelGraph");
+};
+
+const getValues2d = () => {
+  if (graph.value?.getValues2d) return graph.value.getValues2d();
+  throw new TypeError("Variable graph is not instance of VueFunnelGraph");
+};
+
+const percentages = () => {
+  if (graph.value?.createPercentages) return graph.value.createPercentages();
+  throw new TypeError("Variable graph is not instance of VueFunnelGraph");
+};
+
+const twoDimPercentages = () => {
+  if (!is2d()) return [];
+  if (graph.value?.getPercentages2d) return graph.value.getPercentages2d();
+  throw new TypeError("Variable graph is not instance of VueFunnelGraph");
+};
+
+const subLabelBackgrounds = (index) => {
+  if (!is2d()) return [];
+  return generateLegendBackground(
+    getColors.value[index],
+    props.gradientDirection
+  );
+};
+
+const offsetColor = (index, length) => {
+  return `${Math.round((100 * index) / (length - 1))}%`;
+};
+
+const onTransition = (el, done) => {
+  if (!done) return;
+  if (!props.animated) done();
+  setTimeout(() => done(), 700);
+};
 </script>
